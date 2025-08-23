a691eade0d70a75fc6177eba2316c389475b8d9c
raihaniooo <raihanaaaz2@gmail.com>
2025-08-23 15:58:38 +0700

    bisa kirim email dan ada alert popup

--- email.js
+++ email.js
@@ -9,9 +9,23 @@ document.addEventListener("DOMContentLoaded", function () {
 			emailjs
 				.sendForm("service_6udpv8j", "template_idwc7s7", this)
 				.then(() => {
-					alert("Pesan berhasil dikirim!");
+					Swal.fire({
+						icon: "success",
+						title: "Pesan berhasil dikirim!",
+						text: "Terima kasih, saya akan menghubungi Anda dalam 24 jam.",
+						confirmButtonText: "OK",
+						timer: 3000,
+					});
 					this.reset();
 				})
-				.catch((err) => alert("Gagal mengirim: " + JSON.stringify(err)));
+				.catch((err) => {
+					Swal.fire({
+						icon: "error",
+						title: "Gagal mengirim pesan!",
+						text: "Silakan coba lagi. Error: " + err.text,
+						confirmButtonText: "OK",
+						timer: 3000,
+					});
+				});
 		});
 });
